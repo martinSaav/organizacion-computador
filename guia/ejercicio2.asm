f71c4f7391817dbb224f632a542faa6e1fb11d51
Martin Saavedra <martin996@hotmail.com.ar>
2024-06-11 14:19:01 -0300

    ejercicio 2 resuelto y gitignore

--- guia/ejercicio2.asm
+++ guia/ejercicio2.asm
@@ -1,10 +1,10 @@
-%macro mPuts 1
+%macro mPuts 1  
     mov     rdi,%1
     sub     rsp,8
     call    puts
     add     rsp,8
 %endmacro
-
+;genera conflicto con el rsi
 %macro mGets 1
     mov     rdi,%1
     sub     rsp,8
@@ -12,9 +12,12 @@
     add     rsp,8
 %endmacro
 
-%macro mPrintf 0
+%macro mSscanf 3
+    mov     rdi,%1
+    mov     rsi,%2
+    mov     rdx,%3
     sub     rsp,8
-    call    printf
+    call    sscanf
     add     rsp,8
 %endmacro
 
@@ -23,25 +26,33 @@ global main
 extern puts
 extern gets
 extern printf
+extern sscanf
 
 section .data
-    infoAlumnoText             db "El alumno %s de Padrón N° %s tiene %li años",0
-    nombreApellidoInText       db "ingrese su nombre y apellido: ",0
-    padronInText               db "ingrese su padron: ",0
-    fechaNacimientoInText      db "ingrese su fecha de nacimiento con el formato dd/mm/aaaa: ",0 ; si se ingresa mal la fecha de nacimiento, el programa se rompe
-    fechaHoy                   db "15",0
+    infoAlumnoText            db "El alumno %s de Padrón N° %s tiene %li años",10,0
+    nombreApellidoInText      db "ingrese su nombre y apellido: ",0
+    padronInText              db "ingrese su padron: ",0
+    fechaNacimientoInText     db "ingrese su fecha de nacimiento con el formato dd/mm/aaaa: ",0 ; si se ingresa mal la fecha de nacimiento, el programa se rompe
+    diaHoy                    dq 10
+    mesHoy                    dq 6
+    anioHoy                   dq 2024
+    numFormato                db "%d",0
 section .bss
     nombreApellido            resb 500
     padron                    resb 500
     fechaNacimiento           resb 500
-    anios                     resb 1
-    mes                       resb 1
-    dia                       resb 1
-    temp_string               resb 2  ; Temporal para un carácter y el nulo
+    diaNacStr              resb 3   
+    mesNacStr              resb 3   
+    anioNacStr             resb 5
+    diaNac                 resq 1
+    mesNac                 resq 1
+    anioNac                resq 1
+    edad                      resq 1  
 
 section .text
     global main
 main:
+
     mPuts nombreApellidoInText
     mGets nombreApellido
 
@@ -51,28 +62,56 @@ main:
     mPuts fechaNacimientoInText
     mGets fechaNacimiento
 
+    mov     ax,word[fechaNacimiento]    
+    mov     [diaNacStr],ax   
+    mov     byte[diaNacStr+2],0 
 
+    mov     ax,word[fechaNacimiento+3]         
+    mov     [mesNacStr],ax 
+    mov     byte[mesNacStr+2],0 
 
-    sub     rsp,8
-    ;call    calcularEdad
-    add     rsp,8
+    mov     rax,qword[fechaNacimiento+6]
+    mov     [anioNacStr],rax
+    mov     byte[anioNacStr+4],0  
 
+    mSscanf diaNacStr,numFormato,diaNac
 
- ; Iterar sobre fechaHoy y mostrar cada carácter
-    mov     rsi, 0
+    mSscanf mesNacStr,numFormato,mesNac
 
-nextCharFindLast:
-    mov     al, byte[fechaHoy+rsi]          ; Cargar el carácter actual
-    cmp     byte[fechaHoy+rsi], 0              ; Comparar con 0 (fin de cadena)
-    je      endString
+    mSscanf anioNacStr,numFormato,anioNac
 
-    mov     [temp_string],al   ; Colocar el carácter en temp_string
-    mov     byte [temp_string+1], 0 ; Añadir el carácter nulo
-    mPuts   temp_string
+    ; Calcula la edad
+    mov     rax, [anioHoy]
+    sub     rax, [anioNac]
+    mov     [edad], rax
 
-    inc     rsi
-    jmp     nextCharFindLast
+    ; Compara los meses
+    mov     rax, [mesNac]
+    mov     rbx, [mesHoy] ; mesSiendo
+    cmp     rax, rbx
+    jg      decEdad
+    jl      fin
+    je      mesIgual
 
-endString:
+decEdad:
+    sub     qword [edad], 1
+    jmp     fin
+
+mesIgual:
+    ; Compara los días si los meses son iguales
+    mov     rax, [diaHoy]
+    mov     rbx, [diaNac]
+    cmp     rax, rbx
+    jl      decEdad
+
+fin:
+    ; Muestra la información del alumno
+    mov    rdi, infoAlumnoText
+    mov    rsi, nombreApellido
+    mov    rdx, padron
+    mov    rcx, [edad]
+    sub    rsp, 8
+    call   printf
+    add    rsp, 8
     ret
     
